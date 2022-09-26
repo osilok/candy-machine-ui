b331366d49cb12cc84dac85e40b92e98e346d58b
osilok <osilok1984@gmail.com>
2022-09-26 16:49:39 +0300

    animtsol

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 
 
-export const startDate = new Date("19 May 2022 17:00:00 GMT")
-export const startWlDate = new Date("19 May 2022 16:00:00 GMT")
+export const startDate = new Date("29 Sep 2022 17:00:00 GMT")
+export const startWlDate = new Date("27 Sep 2022 16:00:00 GMT")
 
-export const mintPrice = '0.1 SOL'
+export const mintPrice = '0.2 SOL'
 export const supply = 10
